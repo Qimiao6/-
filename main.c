87266ee12f7bc897de18445598ab5479aa7afcec
诸葛斌 <108967191+Qimiao6@users.noreply.github.com>
2022-09-02 21:40:45 +0800

    小题
    小题

--- main.c
+++ main.c
@@ -1,122 +1,114 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-//    数组指针
+//			指针数组
 //#include<stdio.h>
+//void print()
+//{
+//	char i[10] = "djdk";
+//	//i[0] = "jsdj";
+//	printf("%s\n", i);
+//}
 //int main()
 //{
-//	int arr[10] = { 0,1,2,3,4,5,6,7,8,9 };
-//	int(*pp)[10] = &arr;
-//	for (int i = 0; i < 10; i++)
-//	{
-//		printf("%d ", (*pp)[i]);
-//	}
-//	printf("\n");
-//	int* p = arr;//接收首地址
-//	for (int i = 0; i < 10; i++)
-//	{
-//		printf("%d ", *(p+i));
-//	}
+//	int i[4] = { 1,2,3,4 };
+//	int b[4] = { 2,3,4,5 };
+//	int* p[2] = { i , b };
+//	p[0][2] = 9;
+//	printf("%d\n", p[0][2]);
+//	print();
 //	return 0;
 //}
 
-
-//              指针数组
-
+//密码中至少出现大写字母，小写字母和数字这三种字符类型中的两种；
 //#include<stdio.h>
+//#include<string.h>
+//
 //int main()
 //{
-//	int a = 7;
-//	int b = 6;
-//	int* pa = &a;
-//	int* pb = &b;
-//	int* pp[2] = { pa,pb };//指向指针的数组
-//	for (int i = 0; i < 2; i++)
-//	{
-//		printf("%d ", *pp[i]);
-//	}
+//    char i[101];
+//    int n = 0;
+//    int j = 0;
+//    scanf("%d", &n);
+//    for (j = 0; j < n; j++)
+//    {
+//        scanf("%s", i);
+//        int y = 0;
+//        int A = 0;
+//        while (i[y] != '\0')
+//        {
+//            if (i[y] >= 65 && i[y] <= 90)//检测大写
+//            {
+//                A++;//1
+//                break;
+//            }
+//            y++;
+//        }
+//        while (i[y] != '\0')//检测小写
+//        {
+//            if (i[y] >= 97 && i[y] <= 122)
+//            {
+//                A++;//2
+//                break;
+//            }
+//            y++;
+//        }
+//        while (i[y] != '\0')//检测数字
+//        {
+//            if (i[y] <= 9)
+//            {
+//                A++;//3
+//                break;
+//            }
+//            y++;
+//        }
+//        if (strlen(i) >= 8)//检测长度
+//            A++;//4
+//        if (i[0] >= 10)//检测首位是否为数字
+//            A++;//5
+//        if (A == 5)
+//            printf("YES");
+//        else
+//            printf("NO");
+//    }
+//}
+//                  大写变为小写
+//#include <stdio.h>
+//int main()
+//{
+//	char ch[80] = "123abcdEFG*&";
+//	int i = printf("*");
+//	int j;
+//	for (j = 0; ch[j] != '\0'; j++)
+//		if (ch[j] >= 'A' && ch[j] <= 'Z')
+//			ch[j] = ch[j] + 32;
+//			//ch[j] = ch[j] + 'e' - 'E';
+//	printf("%s", ch);
 //	return 0;
 //}
 
-//          左旋右旋
-
+//      统计不同位
 #include<stdio.h>
-void left()
-{
-	int arr[5] = { 0 };
-	int a = 0;
-	int x = 0;
-	printf("请输入一个5位数的数组\n");
-	for (a = 0; a < 5; a++)
-	{
-		scanf("%d", &arr[a]);
-	}
-	int i = 0;
-	printf("请选择左旋转个数\n");
-	scanf("%d", &i);
-	int sz = sizeof(arr)/ sizeof(arr[0]) ;
-	for (a = 0; a < i; a++)
-	{
-		//int b = arr[0];
-		for (x = 0; x < sz - 1-a; x++)
-		{
-			int m = arr[x];
-			arr[x] = arr[x + 1];
-			arr[x + 1] = m;
-		}
-	}
-	for (a = 0; a < sz; a++)
-	{
-		printf("%d ", arr[a]);
-	}
-}
-void rig()
-{
-	int arr[5] = { 0 };
-	int a = 0;
-	int x = 0;
-	printf("请输入一个5位数的数组\n");
-	for (a = 0; a < 5; a++)
-	{
-		scanf("%d", &arr[a]);
-	}
-	int i = 0;
-	printf("请选择右旋转个数\n");
-	scanf("%d", &i);
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	for (a = 0; a < i; a++)
-	{
-		for (x = sz-1; x > 0; x--)
-		{
-			int m = arr[x];
-			arr[x] = arr[x - 1];
-			arr[x - 1] = m;
-		}
-	}
-	for (a = 0; a < sz; a++)
-	{
-		printf("%d ", arr[a]);
-	}
-}
+int convertInteger(int A, int B);
 int main()
 {
-	int i = 0;
-	do
-	{
-		printf("\n********** 请选择模式 *********\n");
-		printf("**********   1 左旋   *********\n");
-		printf("**********   2 右旋   *********\n");
-		scanf("%d", &i);
-		switch (i)
-		{
-		case 1:
-			left();
-			break;
-		case 2:
-			rig();
-			break;
-		case 0:
-			printf("退出");
-			break;
-		}
-	} while (i);
-	
+    int a = 29;
+    int b = 15;
+    convertInteger(a, b);
+    return 0;
+}
+int convertInteger(int A, int B)
+{
+    A = A ^ B;
+    int a = 0;
+    int mm = 0;
+    while (a < 32)
+    {
+        if ((A >> a) & 1 == 1)
+        {
+            mm++;
+        }
+
+        a++;
+    }
+    //printf("%d",mm);
+    return mm;
 }
